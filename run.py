88e0f331ed4e735922642ecce339b5bc178b24c5
LeaGuevara01 <dev.leandro.guevara@gmail.com>
2025-07-14 13:05:25 -0300

    1.1 production onrender!

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 # run.py
-from sistema_gestion_agricola.__init__ import create_app # 'from .' @root/sistema_gestion_agricola/run.py && __init__.py => python -m sistema_gestion_agricola.run
-                                # 'from __init__' @root/sistema_gestion_agricola/run.py && __init__.py => gunicorn run_app
-                                # 'from sistema_gestion_agricola' @root/run.py/sistema_gestion_agricola && __init__.py => python -m sistema_gestion_agricola.run
+from sistema_gestion_agricola.__init__ import create_app
 
 app = create_app()
-app.config['ENV'] = 'development'  # o 'production' en producción
+app.config['ENV'] = 'production'  # 'production' o 'development'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
