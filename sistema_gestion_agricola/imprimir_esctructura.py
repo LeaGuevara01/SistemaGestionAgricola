71bb2b90ae064c5511b49a9cb8fa0aa283f1d6ef
LeaGuevara01 <dev.leandro.guevara@gmail.com>
2025-07-15 13:51:11 -0300

    .python-version 3.11

--- sistema_gestion_agricola/imprimir_esctructura.py
+++ sistema_gestion_agricola/imprimir_esctructura.py
@@ -12,6 +12,6 @@ def imprimir_estructura(base_dir, indent=0):
             imprimir_estructura(path, indent + 1)
 
 # Ruta base (cambiá si hace falta)
-directorio_raiz = 'D:/Code/elorza/sistema_gestion_agricola'
+directorio_raiz = 'D:/Code/elorza/'
 print(f"Estructura de: {directorio_raiz}")
 imprimir_estructura(directorio_raiz)
