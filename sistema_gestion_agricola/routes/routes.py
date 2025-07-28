c1ace1a83e0b79e76258afde953e8bb5965429ea
LeaGuevara01 <dev.leandro.guevara@gmail.com>
2025-07-27 22:43:14 -0300

    Fix routes.py template reference from react_app

--- sistema_gestion_agricola/routes/routes.py
+++ sistema_gestion_agricola/routes/routes.py
@@ -1,32 +1,12 @@
-# sistema_gestion_agricola/routes/routes.py
-from flask import Blueprint, render_template, request, current_app
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    use_react = current_app.config.get('USE_REACT', False)
-    frontend_mode = request.args.get('mode', current_app.config.get('FRONTEND_MODE', 'html'))
-    if use_react or frontend_mode == 'react':
-        return render_template('react_app.html')
-    else:
-        return render_template('index.html')
-
-@main.route('/dashboard')
-def dashboard():
-    use_react = current_app.config.get('USE_REACT', False)
-    frontend_mode = request.args.get('mode', current_app.config.get('FRONTEND_MODE', 'html'))
-    if use_react or frontend_mode == 'react':
-        return render_template('react_app.html')
-    else:
-        return render_template('dashboard.html')
-
-@main.route('/react')
-@main.route('/react/<path:path>')
-def react_app(path=''):
-    return render_template('react_app.html')
-
-@main.route('/html')
-@main.route('/html/<path:path>')
-def html_app(path=''):
+    # Cambiar de 'react_app.html' a 'index.html'
     return render_template('index.html')
+
+@main.route('/vite')
+def vite_app():
+    return render_template('vite.html')
